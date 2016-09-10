0ba34f5da22c6147032071a865167503e445c804
Aidan Fitzgerald <duckjr159@gmail.com>
2016-09-10 15:17:04 -0400

    Moving to GitHub

---
+++ dynamiclist.js
@@ -0,0 +1,36 @@
+/**
+ * AngularJS demo
+ * Copyright (c) 2016 by Aidan Fitzgerald, The Social Hacks Team.
+ * #include <MIT_License.h>
+ */
+
+var socialHacks = angular.module('socialHacks', []);
+
+socialHacks.controller('SponsorListCtl', ['$scope', function($scope) {
+
+  $scope.sponsors = [
+    {
+      "name": "Balsamiq",
+      "url": "https://balsamiq.com/",
+      "logo": "img/balsamiq.svg",
+      "tier": "silver",
+      "blurb": "Balsamiq is the maker of <a href=\"https://balsamiq.com/products/mockups\">Mockups</a> \u2013 the rapid wireframing software that combines the simplicity of paper sketching with the power of a digital tool so that teams can focus on what's important. Balsamiq is a small and personable company that competes on usability and customer service. Balsamiq believes work should be fun, and that life is too short for bad software."
+    },
+    {
+      "name": "Woogle",
+      "url": "http://woogle.example/",
+      "logo": "http://www.gujian.net/woogle/images/woogle.png",
+      "tier": "bronze",
+      "blurb": "Woogle is a made-up company. Apparently it's a product too."
+    }
+  ];
+
+/*  // download the sponsor list in a JSON file...
+  $http.get("sponsors.json").success(function(response) {
+
+    document.write(response);
+    // ... and put it in the scope
+    // $scope.sponsors = JSON.parse(response);
+  });*/
+
+}]);
